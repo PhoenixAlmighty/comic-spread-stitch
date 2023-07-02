277ea6f1f667f7e598ec9eb1aa37eb48be119bc1
Reed Mauzy <rmauzy@gmail.com>
2023-07-02 13:10:42 -0500

    Add code to allow for rotations

--- comicSpreadStitch.py
+++ comicSpreadStitch.py
@@ -46,7 +46,7 @@ def main():
 		
 		if len(parts) > 2 and parts[2].strip() == "manga":
 			manga = True
-		combinePages(imgList, pages, manga)
+		processPages(imgList, pages, manga)
 		
 		imgList = os.listdir()
 		os.chdir("..")
@@ -65,50 +65,83 @@ def main():
 	
 	print("{} books evaluated. See output above for results.\n".format(len(lines)))
 
-def combinePages(imgList, pageList, manga):
+def processPages(imgList, pageList, manga):
 	for page in pageList:
-		# read in the two pages I want to combine
-		# this is page - 1 and page because python lists are 0-indexed and the page numbers are 1-indexed
-		# print("{}, {}".format(page - 1, page))
-		img1 = cv2.imread(imgList[page - 1])
-		img2 = cv2.imread(imgList[page])
+		# rotate without stitching
+		if page[1] == "l" or page[1] == "r":
+			# read in the page I want
+			img = cv2.imread(imgList[page[0] - 1])
+			
+			if page[1] == "l":
+				# rotate left
+				img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+			elif page[1] == "r":
+				# rotate right
+				img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+			
+			# save image
+			cv2.imwrite(imgList[page[0] - 1], img)
 		
-		# horizontally concatenate the two pages
-		if manga:
-			combImg = cv2.hconcat([img2, img1])
+		# stitch and possibly rotate
 		else:
-			combImg = cv2.hconcat([img1, img2])
-		
-		# overwrite the first page with the combined pages
-		cv2.imwrite(imgList[page - 1], combImg)
-		
-		# remove the second page so I don't see it again separately from the combined pages
-		# unless I'm combining the front and back covers, in which case the front cover gets to stay as it is
-		if not page == 0:
-			os.remove(imgList[page])
+			# read in the two pages I want to combine
+			# this is page - 1 and page because python lists are 0-indexed and the page numbers are 1-indexed
+			# print("{}, {}".format(page - 1, page))
+			img1 = cv2.imread(imgList[page[0] - 1])
+			img2 = cv2.imread(imgList[page[0]])
+			
+			# horizontally concatenate the two pages
+			if manga:
+				combImg = cv2.hconcat([img2, img1])
+			else:
+				combImg = cv2.hconcat([img1, img2])
+			
+			# rotate if needed
+			if page[1] == "m":
+				# rotate left
+				combImg = cv2.rotate(combImg, cv2.ROTATE_90_COUNTERCLOCKWISE)
+			elif page[1] == "s":
+				# rotate right
+				combImg = cv2.rotate(combImg, cv2.ROTATE_90_CLOCKWISE)
+			
+			# overwrite the first page with the combined pages
+			cv2.imwrite(imgList[page[0] - 1], combImg)
+			
+			# remove the second page so I don't see it again separately from the combined pages
+			# unless I'm combining the front and back covers, in which case the front cover gets to stay as it is
+			if not page[0] == 0:
+				os.remove(imgList[page[0]])
 
 def printSuccess(bookFileName, pagesList):
 	pagesString = ""
-	if pagesList[0] == 0 and len(pagesList) == 1:
+	pagesDeleted = 0
+	if pagesList[0][0] == 0 and len(pagesList) == 1:
 		pagesString = "the back cover"
 		del pagesList[0]
-	elif pagesList[0] == 0 and len(pagesList) > 1:
+	elif pagesList[0][0] == 0 and len(pagesList) > 1:
 		pagesString = "the back cover and "
 		del pagesList[0]
 	numPages = len(pagesList)
 	if numPages == 1:
-		pagesString += "page {}".format(pagesList[0])
+		pagesString += "page {}".format(pagesList[0][0])
 	elif numPages == 2:
-		pagesString += "pages {} and {}".format(pagesList[0], pagesList[1] - 1)
+		if not pagesList[0][1] == "l" or not pagesList[0][1] == "r":
+			pagesDeleted = 1
+		pagesString += "pages {} and {}".format(pagesList[0][0], pagesList[1][0] - pagesDeleted)
 	elif numPages > 2:
 		pagesString += "pages "
 		for i in range(numPages):
 			if i == numPages - 1:
-				pagesString += "{}".format(pagesList[i] - i)
+				pagesString += "{}".format(pagesList[i][0] - pagesDeleted)
+				# Not incrementing pagesDeleted because there's no more for it to affect
 			elif i == numPages - 2:
-				pagesString += "{}, and ".format(pagesList[i] - i)
+				pagesString += "{}, and ".format(pagesList[i][0] - pagesDeleted)
+				if not pagesList[0][1] == "l" or not pagesList[0][1] == "r":
+					pagesDeleted++
 			else:
-				pagesString += "{}, ".format(pagesList[i] - i)
+				pagesString += "{}, ".format(pagesList[i][0] - pagesDeleted)
+				if not pagesList[0][1] == "l" or not pagesList[0][1] == "r":
+					pagesDeleted++
 	
 	print("{} successfully altered on {}.".format(bookFileName, pagesString))
 
@@ -123,18 +156,29 @@ def bookDirIsValid(bookDir):
 	return True
 
 def convertPageList(pageString, bookDir):
+	# validate that input has pages to combine
 	if pageString == "":
 		print("{} has no pages to combine. Check your input.".format(bookDir))
 		return False
+	
+	# add pages to list of ints while validating that each page has a number and, if any modifiers, ones that match what's defined
 	pageStringList = pageString.split(",")
 	pageIntList = []
 	for page in pageStringList:
 		page = page.strip()
+		lastChar = page[-1]
+		if not lastChar.isdigit():
+			if not lastChar == "r" and not lastChar == "s" and not lastChar == "l" and not lastChar == "m":
+				print("Page list for {} contains at least one thing that's not a number and doesn't match any of the available per-page commands. Check your input.".format(bookDir))
+				return False
+			page = page[:-1]
+		else:
+			lastChar = ""
 		if not page.isdigit():
-			print("Page list for {} contains at least one thing that's not a number. Check your input.".format(bookDir))
+			print("Page list for {} contains at least one thing that's not a number and doesn't match any of the available per-page commands. Check your input.".format(bookDir))
 			return False
 		else:
-			pageIntList.append(int(page))
+			pageIntList.append([int(page), lastChar])
 	
 	pageIntList.sort()
 	
--- test_comicSpreadStitch.py
+++ test_comicSpreadStitch.py
@@ -9,51 +9,74 @@ class TestComicSpreadStitch(unittest.TestCase):
 		# Back cover only
 		capturedOutput = io.StringIO()
 		sys.stdout = capturedOutput
-		comicSpreadStitch.printSuccess("Test book", [0])
+		comicSpreadStitch.printSuccess("Test book", [[0, ""]])
 		sys.stdout = sys.__stdout__
 		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on the back cover.\n", "Console output is wrong for back cover only.")
 		
 		# Back cover + 1 spread
 		capturedOutput = io.StringIO()
 		sys.stdout = capturedOutput
-		comicSpreadStitch.printSuccess("Test book", [0, 2])
+		comicSpreadStitch.printSuccess("Test book", [[0, ""], [2, ""]])
 		sys.stdout = sys.__stdout__
 		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on the back cover and page 2.\n", "Console output is wrong for back cover plus 1 spread.")
 		
 		# Back cover + 2 spreads
 		capturedOutput = io.StringIO()
 		sys.stdout = capturedOutput
-		comicSpreadStitch.printSuccess("Test book", [0, 2, 4])
+		comicSpreadStitch.printSuccess("Test book", [[0, ""], [2, ""], [4, ""]])
 		sys.stdout = sys.__stdout__
 		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on the back cover and pages 2 and 3.\n", "Console output is wrong for back cover plus 2 spreads.")
 		
 		# Back cover + 3 or more spreads
 		capturedOutput = io.StringIO()
 		sys.stdout = capturedOutput
-		comicSpreadStitch.printSuccess("Test book", [0, 2, 4, 6])
+		comicSpreadStitch.printSuccess("Test book", [[0, ""], [2, ""], [4, ""], [6, ""]])
 		sys.stdout = sys.__stdout__
 		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on the back cover and pages 2, 3, and 4.\n", "Console output is wrong for back cover plus 3 spreads.")
 		
 		# 1 spread
 		capturedOutput = io.StringIO()
 		sys.stdout = capturedOutput
-		comicSpreadStitch.printSuccess("Test book", [2])
+		comicSpreadStitch.printSuccess("Test book", [[2, ""]])
 		sys.stdout = sys.__stdout__
 		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on page 2.\n", "Console output is wrong for 1 spread.")
 		
 		# 2 spreads
 		capturedOutput = io.StringIO()
 		sys.stdout = capturedOutput
-		comicSpreadStitch.printSuccess("Test book", [2, 4])
+		comicSpreadStitch.printSuccess("Test book", [[2, ""], [4, ""]])
 		sys.stdout = sys.__stdout__
 		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on pages 2 and 3.\n", "Console output is wrong for 2 spreads.")
 		
 		# 3 or more spreads
 		capturedOutput = io.StringIO()
 		sys.stdout = capturedOutput
-		comicSpreadStitch.printSuccess("Test book", [2, 4, 6])
+		comicSpreadStitch.printSuccess("Test book", [[2, ""], [4, ""], [6, ""]])
 		sys.stdout = sys.__stdout__
 		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on pages 2, 3, and 4.\n", "Console output is wrong for 3 spreads.")
+		
+		### No modifiers above this line
+		
+		# 2 spreads, 1 rotation
+		capturedOutput = io.StringIO()
+		sys.stdout = capturedOutput
+		comicSpreadStitch.printSuccess("Test book", [[2, ""], [4, "l"], [6, ""]])
+		sys.stdout = sys.__stdout__
+		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on pages 2, 3, and 5.\n", "Console output is wrong for 2 spreads and 1 rotation.")
+		
+		# 3 spreads, 1 of which is rotated
+		capturedOutput = io.StringIO()
+		sys.stdout = capturedOutput
+		comicSpreadStitch.printSuccess("Test book", [[2, ""], [4, "m"], [6, ""]])
+		sys.stdout = sys.__stdout__
+		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on pages 2, 3, and 4.\n", "Console output is wrong for 3 spreads, 1 of which has been rotated.")
+		
+		# Check that all modifiers are handled correctly
+		capturedOutput = io.StringIO()
+		sys.stdout = capturedOutput
+		comicSpreadStitch.printSuccess("Test book", [[2, ""], [4, "m"], [6, "l"], [8, "r"], [10, "s"], [12, ""]])
+		sys.stdout = sys.__stdout__
+		self.assertEqual(capturedOutput.getvalue(), "Test book successfully altered on pages 2, 3, 4, 6, 8, and 9.\n", "At least one modifier has been handled incoreectly.")
 	
 	def test_bookDirIsValid(self):
 		# No book directory
@@ -86,19 +109,33 @@ class TestComicSpreadStitch(unittest.TestCase):
 		self.assertFalse(pageList, "Should return False if string is empty.")
 		self.assertEqual(capturedOutput.getvalue(), "Test book directory has no pages to combine. Check your input.\n", "Console output is incorrect for empty page list.")
 		
-		# Letters in list
+		# Letters in list that aren't attached to numbers
 		capturedOutput = io.StringIO()
 		sys.stdout = capturedOutput
 		pageList = comicSpreadStitch.convertPageList("1,2,a", "Test book directory")
 		sys.stdout = sys.__stdout__
-		self.assertFalse(pageList, "Should return False if string contains letters.")
-		self.assertEqual(capturedOutput.getvalue(), "Page list for Test book directory contains at least one thing that's not a number. Check your input.\n", "Console output is incorrect for non-numeric page list.")
+		self.assertFalse(pageList, "Should return False if string contains letters that aren't attached to numbers.")
+		self.assertEqual(capturedOutput.getvalue(), "Page list for Test book directory contains at least one thing that's not a number and doesn't match any of the available per-page commands. Check your input.\n", "Console output is incorrect for non-numeric page list.")
+		
+		# Letters in list that don't match the defined modifiers
+		capturedOutput = io.StringIO()
+		sys.stdout = capturedOutput
+		pageList = comicSpreadStitch.convertPageList("1,2,3a", "Test book directory")
+		sys.stdout = sys.__stdout__
+		self.assertFalse(pageList, "Should return False if string contains letters that aren't attached to numbers.")
+		self.assertEqual(capturedOutput.getvalue(), "Page list for Test book directory contains at least one thing that's not a number and doesn't match any of the available per-page commands. Check your input.\n", "Console output is incorrect for non-defined modifiers.")
 		
 		# Only numbers in list
-		self.assertEqual(comicSpreadStitch.convertPageList("5,3,7", "Test book directory"), [3, 5, 7], "Should return a sorted list of integers.")
+		self.assertEqual(comicSpreadStitch.convertPageList("5,3,7", "Test book directory"), [[3, ""], [5, ""], [7, ""]], "Should return a sorted list of lists, where each list is an integer followed by an empty string.")
 		
-		# Only numbers in list with spaces
-		self.assertEqual(comicSpreadStitch.convertPageList("5, 3, 7 ", "Test book directory"), [3, 5, 7], "Should return a sorted list of integers without the whitespace triggering a return value of False.")
+		# Only numbers and spaces in list
+		self.assertEqual(comicSpreadStitch.convertPageList("5, 3, 7 ", "Test book directory"), [[3, ""], [5, ""], [7, ""]], "Should return a sorted list of lists, where each list is an integer followed by an empty string, without the whitespace triggering a return value of False.")
+		
+		# Only numbers and modifiers in list
+		self.assertEqual(comicSpreadStitch.convertPageList("5l,3r,9s,7m", "Test book directory"), [[3, "r"], [5, "l"], [7, "m"], [9, "s"]], "Should return a sorted list of lists, where each list is an integer followed by one of the following letters: l, m, r, s.")
+		
+		# Numbers, modifiers, and spaces in list
+		self.assertEqual(comicSpreadStitch.convertPageList("5l, 3r, 9s, 7m", "Test book directory"), [[3, "r"], [5, "l"], [7, "m"], [9, "s"]], "Should return a sorted list of lists, where each list is an integer followed by one of the following letters: l, m, r, s, without the whitespace triggering a return value of False.")
 	
 	def test_findCBZFile(self):
 		# Directory has no CBZ file
