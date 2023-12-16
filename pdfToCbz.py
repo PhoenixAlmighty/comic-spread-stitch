82c9955ec16bde69c609cd35a7f138f4d0be5d3c
Reed Mauzy <rmauzy@gmail.com>
2023-12-16 17:04:29 -0600

    Finish basic PDF-to-CBZ function

--- pdfToCbz.py
+++ pdfToCbz.py
@@ -3,6 +3,7 @@ from zipfile import ZipFile
 import argparse
 import os
 import math
+import shutil
 
 tempPath = "temp"
 newPath = "new"
@@ -27,16 +28,31 @@ def convertPdfToCbz(book):
 	reader = PdfReader(bookPdf)
 	count = 0
 	numDigits = math.ceil(math.log(len(reader.pages), 10))
+	cbzFileName = os.path.basename(root) + ".cbz"
+	
 	try:
 		os.mkdir(tempPath)
 	except FileExistsError as err:
 		print(tempPath + " directory already exists")
 	
+	# The reason for the line image_file_object = page.images[count] is that
+	# the PDF I first tried this on seemed to have all the images repeated in each page
 	for page in reader.pages:
-		for image_file_object in page.images:
-			with open(os.path.join(tempPath, ("{:0" + str(numDigits) + "d}").format(count) + os.path.splitext(image_file_object.name)[1]), "wb") as fp:
-				fp.write(image_file_object.data)
-				count += 1
+		# for image_file_object in page.images:
+		image_file_object = page.images[count]
+		with open(os.path.join(tempPath, ("{:0" + str(numDigits) + "d}").format(count) + os.path.splitext(image_file_object.name)[1]), "wb") as fp:
+			fp.write(image_file_object.data)
+			count += 1
+	
+	imgs = os.listdir(tempPath)
+	with ZipFile(cbzFileName, "w") as cbz:
+		for img in imgs:
+			cbz.write(os.path.join(tempPath, img), arcname = img)
+	
+	# clean up
+	shutil.rmtree(tempPath)
+	
+	print("{} converted to CBZ.".format(bookPdf))
 
 if __name__ == "__main__":
 	main()
