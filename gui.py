07e64e7d0deb185d0b1c6997ccc479c21cc70582
Reed Mauzy <rmauzy@gmail.com>
2024-09-14 12:55:45 -0500

    Add template for gui.py

---
+++ gui.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+def main():
+    window = tk.Tk()
+
+    # build window contents here
+    # needs:
+        # an entry box for the file name and path, preferably with a browse button
+        # an entry box for the pages to process
+        # a button to process the file
+        # a label to show the results of the processing
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
